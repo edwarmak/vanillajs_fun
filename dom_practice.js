5d16479d0bd426541cea4b9f6a7fe7333efa4d12
Makayla Edwards <mackedwards@Makaylas-MacBook-Air.local>
2022-06-02 10:38:18 -0600

    fun vanilla js

---
+++ dom_practice.js
@@ -0,0 +1,14 @@
+// Get the element with the id you assigned and save it to a variable
+// Console.log that variable.
+let image = document.querySelector('#container')
+console.log(image)
+// Get the h2 element and save it to a variable h2.
+// Console.log that variable.
+let h2 = document.querySelector('h2')
+console.log(h2)
+// Using JavaScript, change the innerText of your h2 to have
+// 3 exclamation points at the end
+document.querySelector('h2').innerText = 'Welcome to West Jordan!!!'
+// Get all of the elements with the class you assigned and
+// save them to a variable. Console.log that variable.
+
