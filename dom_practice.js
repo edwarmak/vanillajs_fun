bf6e2a2b79538603d306f060b140189484ae81ce
Makayla Edwards <mackedwards@Makaylas-MacBook-Air.local>
2022-06-02 10:49:03 -0600

    changing CSS with vanilla js

--- dom_practice.js
+++ dom_practice.js
@@ -11,4 +11,11 @@ console.log(h2)
 document.querySelector('h2').innerText = 'Welcome to West Jordan!!!'
 // Get all of the elements with the class you assigned and
 // save them to a variable. Console.log that variable.
-
+let section = document.querySelectorAll('.section')
+console.log(section)
+//  Change the style/color of the text of your ONE of your li elements
+// to your favorite html color
+document.querySelector('ol').children[0].style.color = 'DarkOliveGreen'
+// Change the innerText of one of your list items to
+// 'Jamboree sandwich at the Jambo-ree'
+document.querySelector('ol').children[1].innerText = 'Jamboree sandwich at the Jambo-ree'
